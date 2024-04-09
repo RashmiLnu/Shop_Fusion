be0f5c27635546afc8a887ddf0444811b904bc7e
sg1605 <sg1605@usnh.edu>
2024-04-08 23:23:46 -0400

    create html file in templates

---
+++ index_app/urls.py
@@ -0,0 +1,16 @@
+
+
+# from django.contrib import admin
+# from django.urls import path
+# from index_app import views
+
+# urlpatterns = [
+#    path("home/", views.home, name="home"),
+# ]
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("home", views.home, name="home"),
+    
+]
