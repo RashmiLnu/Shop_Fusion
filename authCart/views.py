b1a0b2839309a02f07fb0a9f894031dfc325b222
RashmiLnu <Rashmi.Lnu@unh.edu>
2024-04-22 20:19:49 -0400

    create login page with for an existing user

--- authCart/views.py
+++ authCart/views.py
@@ -16,20 +16,41 @@ def signUp(request):
         email = request.POST.get("email")
         password = request.POST.get("pass1")
         confirm_password = request.POST.get("pass2")
-
+        
+       
+        
         if password != confirm_password:
             messages.error(request, "Passwords do not match")
-            return redirect("signUp")
+            return redirect("/authCart/signUp")
+        
+         # Check if a user with this email already exists
+        if User.objects.filter(username=email).exists():
+            messages.error(request, "A user with this email already exists.")
+            return redirect('/authCart/login')
         
         user = User.objects.create_user(email, email, password)
+        messages.success(request, "Account created successfully")
         user.is_active = True
         user.save()
+        
         
 
     return render(request, "authentication/signup.html")
 
 
 def handle_login(request):
+    if request.method == "POST":
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+
+        user = authenticate(username=email, password=password)
+        if user is not None:
+            login(request, user)
+            messages.success(request, "Logged in successfully")
+            return redirect("/")
+        else:
+            messages.error(request, "Invalid credentials or User do not exist.")
+            return redirect("/authCart/login")
     return render(request, "authentication/login.html")
 
 
