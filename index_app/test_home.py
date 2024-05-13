444968150f903a912acc3d6345f6e4945076eb10
RashmiLnu <Rashmi.Lnu@unh.edu>
2024-05-13 17:24:33 -0400

    Add test case for the signUp

--- index_app/test_home.py
+++ index_app/test_home.py
@@ -10,7 +10,7 @@ class HomeViewTestCase(TestCase):
         self.client = Client()
         self.home_url = reverse('home')
 
-        with open(r"C:\Users\unhmguest\Downloads\Female_jeans.jfif", 'rb') as file:
+        with open(r"../../Downloads/Female_jeans.jfif", 'rb') as file:
             image = SimpleUploadedFile(name='Female_jeans.jfif', content=file.read(), content_type='image/jpeg')
 
         Product.objects.create(product_name='Test Product 1', product_category='Test Category 1', product_image=image)
@@ -22,5 +22,8 @@ class HomeViewTestCase(TestCase):
 
         self.assertEqual(response.status_code, 200)
         self.assertEqual(len(allProds), 2)
-        self.assertEqual(allProds[0][0][0].product_name, 'Test Product 1')
-        self.assertEqual(allProds[1][0][0].product_name, 'Test Product 2')
+
+        # Check that 'Test Product 1' and 'Test Product 2' are in allProds, regardless of their order
+        product_names = [prod[0][0].product_name for prod in allProds]
+        self.assertIn('Test Product 1', product_names)
+        self.assertIn('Test Product 2', product_names)
