360e457d10ba57288fd82e7e37348c3a583d512d
SindhuraMallepoola <sindhura.mallepoola@unh.edu>
2024-05-13 14:50:15 -0400

    Revert "Modify code for signup page."
    This reverts commit 0e79389f39dabec72ba7140bfcd3049bd06279f6.

--- authCart/views.py
+++ authCart/views.py
@@ -29,7 +29,6 @@ def signUp(request):
         messages.success(request, "Account created successfully")
         user.is_active = True
         user.save()
-        return render(request, "authentication/login.html")
 
     return render(request, "authentication/signup.html")
 
